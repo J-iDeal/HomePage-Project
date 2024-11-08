8c06774b4d05a155b67f99655b71603dea0ff213
J-iDeal <133293049+J-iDeal@users.noreply.github.com>
2024-11-09 03:09:15 +0800

    修改base仓库名

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path';
 
 export default defineConfig({
-  base: '/iDeal-HomePage/', // 替换成gitHub仓库名
+  base: '/HomePage-Project/', // 替换成gitHub仓库名
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),  // 这将“@”映射到“src”文件夹
